3a1457cf90d5b91a26daa576753496614a78fb55
jzfan <jzmcc@163.com>
2019-04-18 17:55:08 +0800

    秒杀商品详情接口返回多图片

--- application/common/model/SeckillGoods.php
+++ application/common/model/SeckillGoods.php
@@ -2,40 +2,50 @@
 
 namespace app\common\model;
 
+use app\common\ModelTrait;
 use think\Db;
 use think\Model;
-use app\common\ModelTrait;
 
 class SeckillGoods extends Model
 {
-	use ModelTrait;
+    use ModelTrait;
 
-	public function sku()
-	{
-		return $this->belongsTo(Goods::class, 'goods_id', 'goods_id');	
-	}
+    public function sku()
+    {
+        return $this->belongsTo(Goods::class, 'goods_id', 'goods_id');
+    }
 
-	public function info()
-	{
-		return $this->belongsTo(Goods::class, 'goods_id', 'goods_id')->field('goods_id,goods_name,goods_image');
-	}
+    public function info()
+    {
+        return $this->belongsTo(Goods::class, 'goods_id', 'goods_id')->field('goods_id,goods_commonid,goods_name,goods_image');
+    }
 
-	public function byId($id)
-	{
-		return $this->with('info')->find($id)->format();
-	}
+    public function byId($id)
+    {
+        return $this->with('info')->find($id);
+    }
 
-	public function format()
-	{
-		return [
-	        'id' => $this->id,
-	        'name' => $this->info->goods_name,
-	        'image' => $this->info->goods_image,
-	        'price' => $this->price,
-	        'mi' => $this->mi,
-	        'qty' => $this->qty,
-	        'sold' => $this->sold
-	    ];
-	}
+    public function commonId()
+    {
+        return $this->sku->goods_commonid;
+    }
+
+    public function images()
+    {
+        return db('goodsimages')->where('goods_commonid', $this->commonId())->order('goodsimage_isdefault,goodsimage_sort')->column('goodsimage_url');
+    }
+
+    public function format()
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->info->goods_name,
+            'images' => join(',', $this->images()),
+            'price' => $this->price,
+            'mi' => $this->mi,
+            'qty' => $this->qty,
+            'sold' => $this->sold,
+        ];
+    }
 
 }
