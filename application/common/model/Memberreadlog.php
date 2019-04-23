22310b0efb8b9633af2bd110bee71818f80d4bc2
hb_wanghaibo <920793414@qq.com>
2019-04-23 10:19:15 +0800

    文章接口

---
+++ application/common/model/Memberreadlog.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by 91go.miaozancn.com
+ * @Date: 2019/4/23
+ * @Time: 09:53
+ * @auther: mark_haibo
+ * @email:920793414@qq.com
+ * @File:Memberreadlog.php
+ */
+
+namespace app\common\model;
+
+use think\Model;
+
+class Memberreadlog extends Model
+{
+    public $page_info;
+
+}
