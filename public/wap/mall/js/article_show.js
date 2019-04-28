5198bc8141249dbacf187198412e23eedaef00b6
heyang <244251267@qq.com>
2019-04-28 11:23:31 +0800

    【文章】

--- public/wap/mall/js/article_show.js
+++ public/wap/mall/js/article_show.js
@@ -1,18 +1,20 @@
+var e = getCookie("key");
 $(function() {
+	if(!e){
+		e="";
+	}
+	
     var article_id = getQueryString('article_id');
     if (article_id == '') {
-//      window.location.href = WapSiteUrl + '/index.html';
-        return;
+        return false;
     }else {
         $.ajax({
             url: ApiUrl + "/Article/article_show.html",
             type: 'get',
-            data: {article_id: article_id},
-            jsonp: 'callback',
-            dataType: 'jsonp',
+            data: {article_id: article_id,key:e},
+            dataType: 'json',
             success: function(result) {
             	$(".header-title h1").text(result.result.article_title);
-            	
                 var data = result.result;
                 var html = template('article', data);
                 $("#article-content").html(html);
