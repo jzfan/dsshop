9f09ec8567fdd119f8399cac64a65a2722aaa209
heyang <244251267@qq.com>
2019-04-28 17:30:30 +0800

    【订单页面】

--- public/wap/member/js/pointorder_list.js
+++ public/wap/member/js/pointorder_list.js
@@ -1,42 +1,45 @@
-var page = pagesize;
 var curpage = 1;
 var hasMore = true;
 var footer = false;
 var reset = true;
 var orderKey = "";
-$(function() { 
+$(function() {
     var e = getCookie("key");
     if (!e) {
         window.location.href = WapSiteUrl + "/member/login.html"
     }
     if (getQueryString("data-state") != "") {
-        $("#filtrate_ul").find("li").has('a[data-state="' + getQueryString("data-state") + '"]').addClass("selected").siblings().removeClass("selected")
+        $("#filtrate_ul").find("li").has('a[data-state="' + getQueryString("data-state") + '"]').addClass("selected").siblings().removeClass("selected");
     }
     $("#search_btn").click(function() {
         reset = true;
-        t()
+        t();
     });
     $("#fixed_nav").waypoint(function() {
-        $("#fixed_nav").toggleClass("fixed")
+        $("#fixed_nav").toggleClass("fixed");
     },
     {
         offset: "50"
     });
     function t() {
         if (reset) {
+            $("#order-list").empty();
             curpage = 1;
-            hasMore = true
+            hasMore = true;
+            reset = false;
         }
+        if(!hasMore){
+            return false;
+        }
+
         $(".loading").remove();
-        if (!hasMore) {
-            return false
-        }
-        hasMore = false;
+
         var t = $("#filtrate_ul").find(".selected").find("a").attr("data-state");
         var r = $("#order_key").val();
+        
         $.ajax({
             type: "post",
-            url: ApiUrl + "/Memberpointorder/orderlist.html?pagesize=" + pagesize + "&page=" + curpage,
+            url: ApiUrl + "/Memberorder/order_list.html?page=" + curpage + "&pagesize=" + pagesize,
             data: {
                 key: e,
                 state_type: t,
@@ -47,11 +50,11 @@ $(function() {
             success: function(e) {
                 checkLogin(e.login);
                 curpage++;
-                hasMore = e.hasmore;
+                hasMore = e.result.hasmore;
                 if (!hasMore) {
                     get_footer()
                 }
-                if (e.result.order_list.length <= 0) {
+                if (e.result.order_group_list.length <= 0) {
                     $("#footer").addClass("posa")
                 } else {
                     $("#footer").removeClass("posa")
@@ -60,8 +63,7 @@ $(function() {
                 t.WapSiteUrl = WapSiteUrl;
                 t.ApiUrl = ApiUrl;
                 t.key = getCookie("key");
-                template.helper("$getLocalTime",
-                function(e) {
+                template.helper("$getLocalTime",function(e) {
                     var t = new Date(parseInt(e) * 1e3);
                     var r = "";
                     r += t.getFullYear() + "年";
@@ -71,29 +73,94 @@ $(function() {
                     r += t.getMinutes();
                     return r
                 });
-                template.helper("p2f",
-                function(e) {
-                    return (parseFloat(e) || 0).toFixed(2)
+                template.helper("p2f",function(e) {
+                    return (parseFloat(e) || 0).toFixed(2);
                 });
-                template.helper("parseInt",
-                function(e) {
-                    return parseInt(e)
+                template.helper("parseInt",function(e) {
+                    return parseInt(e);
                 });
                 var r = template("order-list-tmpl", t);
-                if (reset) {
-                    reset = false;
-                    $("#order-list").html(r)
+
+                if (!reset) {
+                    $("#order-list").append(r);
                 } else {
-                    $("#order-list").append(r)
+                    $("#order-list").html(r);
                 }
             }
         })
     }
-   
-    $("#order-list").on("click", ".sure-order", n);
     $("#order-list").on("click", ".cancel-order", r);
-    $("#order-list").on("click", ".view-order", c);
-
+    $("#order-list").on("click", ".delete-order", o);
+    $("#order-list").on("click", ".sure-order", n);
+    $("#order-list").on("click", ".evaluation-order", l);
+    $("#order-list").on("click", ".evaluation-again-order", d);
+    $("#order-list").on("click", ".viewdelivery-order", c);
+    $("#order-list").on("click", ".check-payment",
+    function() {
+        var e = $(this).attr("data-paySn");
+        toPay(e, "memberbuy", "pay");
+        return false;
+    });
+    function r() {
+        var e = $(this).attr("order_id");
+        layer.open({
+            content: '确定取消订单？'
+            , btn: ['确定','取消']
+            , yes: function (index) {
+                a(e)
+                layer.close(index);
+            }
+        });
+    }
+    function a(r) {
+        $.ajax({
+            type: "post",
+            url: ApiUrl + "/Memberorder/order_cancel.html",
+            data: {
+                order_id: r,
+                key: e
+            },
+            dataType: "json",
+            success: function(e) {
+                if (e.code == 10000) {
+                    reset = true;
+                    t()
+                } else {
+                    layer.open({content: e.message, btn: '我知道了'});
+                }
+            }
+        })
+    }
+    function o() {
+        var e = $(this).attr("order_id");
+        layer.open({
+            content: '是否移除订单？电脑端订单回收站可找回订单！'
+            , btn: ['确定','取消']
+            , yes: function (index) {
+                i(e)
+                layer.close(index);
+            }
+        });
+    }
+    function i(r) {
+        $.ajax({
+            type: "post",
+            url: ApiUrl + "/Memberorder/order_delete.html",
+            data: {
+                order_id: r,
+                key: e
+            },
+            dataType: "json",
+            success: function(e) {
+                if (e.code == 10000) {
+                    reset = true;
+                    t()
+                } else {
+                    layer.open({content: e.message, btn: '我知道了'});
+                }
+            }
+        })
+    }
     function n() {
         var e = $(this).attr("order_id");
         layer.open({
@@ -108,70 +175,46 @@ $(function() {
     function s(r) {
         $.ajax({
             type: "post",
-            url: ApiUrl + "/Memberpointorder/receiving_order.html",
+            url: ApiUrl + "/Memberorder/order_receive.html",
             data: {
                 order_id: r,
                 key: e
             },
             dataType: "json",
             success: function(e) {
-                if (e.result && e.result == 1) {
-                    reset = true;
-                    t();
-                } else {
-                    layer.open({content: e.message,skin: 'msg',time: 2});
-                }
-            }
-        })
-    }
-
-    function r() {
-        var e = $(this).attr("order_id");
-        layer.open({
-            content: '确定取消兑换？'
-            , btn: ['确定','取消']
-            , yes: function (index) {
-                a(e)
-                layer.close(index);
-            }
-        });
-    }
-    function a(r) {
-        $.ajax({
-            type: "post",
-            url: ApiUrl + "/Memberpointorder/cancel_order.html",
-            data: {
-                order_id: r,
-                key: e
-            },
-            dataType: "json",
-            success: function(e) {
-                if (e.result && e.result == 1) {
+                if (e.code == 10000) {
                     reset = true;
                     t()
                 } else {
-                    layer.open({content: e.message,skin: 'msg',time: 2});
+                    layer.open({content: e.message, btn: '我知道了'});
                 }
             }
         })
     }
-	
-	
+    function l() {
+        var e = $(this).attr("order_id");
+        location.href = WapSiteUrl + "/member/member_evaluation.html?order_id=" + e
+    }
+    function d() {
+        var e = $(this).attr("order_id");
+        location.href = WapSiteUrl + "/member/member_evaluation_again.html?order_id=" + e
+    }
     function c() {
         var e = $(this).attr("order_id");
-        location.href = WapSiteUrl + "/member/pointorder_view.html?order_id=" + e;
+        location.href = WapSiteUrl + "/member/order_delivery.html?order_id=" + e
     }
     $("#filtrate_ul").find("a").click(function() {
         $("#filtrate_ul").find("li").removeClass("selected");
         $(this).parent().addClass("selected").siblings().removeClass("selected");
         reset = true;
         window.scrollTo(0, 0);
-        t()
+        t();
     });
     t();
     $(window).scroll(function() {
-        if ($(window).scrollTop() + $(window).height() > $(document).height() - 1) {
-            t()
+        if ($(window).scrollTop() + $(window).height() > $(document).height() - 10) {
+            //console.log($(window).scrollTop() + $(window).height());
+            t();
         }
     })
 });
--- public/wap/member/js/vr_order_list.js
+++ public/wap/member/js/vr_order_list.js
@@ -2,140 +2,139 @@ var curpage = 1;
 var hasMore = true;
 var footer = false;
 var reset = true;
-var page = pagesize;
 var orderKey = "";
-$(function () {
+$(function() {
     var e = getCookie("key");
     if (!e) {
         window.location.href = WapSiteUrl + "/member/login.html"
     }
     if (getQueryString("data-state") != "") {
-        $("#filtrate_ul").find("li").has('a[data-state="' + getQueryString("data-state") + '"]').addClass("selected").siblings().removeClass("selected")
+        $("#filtrate_ul").find("li").has('a[data-state="' + getQueryString("data-state") + '"]').addClass("selected").siblings().removeClass("selected");
     }
-    var t = false;
-    var a = false;
-    $("#search_btn").click(function () {
+    $("#search_btn").click(function() {
         reset = true;
-        r()
+        t();
     });
-    $("#fixed_nav").waypoint(function () {
-            $("#fixed_nav").toggleClass("fixed")
-        },
-        {
-            offset: "50"
-        });
-
-    function r() {
+    $("#fixed_nav").waypoint(function() {
+        $("#fixed_nav").toggleClass("fixed");
+    },
+    {
+        offset: "50"
+    });
+    function t() {
         if (reset) {
+            $("#order-list").empty();
             curpage = 1;
             hasMore = true;
-            $("#footer").html("")
+            reset = false;
         }
+        if(!hasMore){
+            return false;
+        }
+
         $(".loading").remove();
-        if (!hasMore) {
-            return false
-        }
-        hasMore = false;
+
         var t = $("#filtrate_ul").find(".selected").find("a").attr("data-state");
-        var a = $("#order_key").val();
+        var r = $("#order_key").val();
+        
         $.ajax({
             type: "post",
-            url: ApiUrl + "/Membervrorder/order_list.html?page=" + curpage + "&pagesize=" + pagesize,
+            url: ApiUrl + "/Memberorder/order_list.html?page=" + curpage + "&pagesize=" + pagesize,
             data: {
                 key: e,
                 state_type: t,
                 order_type: $(".cur").attr("order_type"),
-                order_key: a
+                order_key: r
             },
             dataType: "json",
-            success: function (e) {
+            success: function(e) {
                 checkLogin(e.login);
                 curpage++;
-                hasMore = e.hasmore;
+                hasMore = e.result.hasmore;
                 if (!hasMore) {
                     get_footer()
                 }
-                if (e.result.order_list.length <= 0) {
+                if (e.result.order_group_list.length <= 0) {
                     $("#footer").addClass("posa")
+                } else {
+                    $("#footer").removeClass("posa")
                 }
-                var t = e.result;
+                var t = e;
                 t.WapSiteUrl = WapSiteUrl;
                 t.ApiUrl = ApiUrl;
                 t.key = getCookie("key");
-                    
-                template.helper("$getLocalTime",
-                    function (e) {
-                        var t = new Date(parseInt(e) * 1e3);
-                        var a = "";
-                        a += t.getFullYear() + "年";
-                        a += t.getMonth() + 1 + "月";
-                        a += t.getDate() + "日 ";
-                        a += t.getHours() + ":";
-                        a += t.getMinutes();
-                        return a
-                    });
-                template.helper("p2f",
-                    function (e) {
-                        return (parseFloat(e) || 0).toFixed(2)
-                    });
-                template.helper("parseInt",
-                    function (e) {
-                        return parseInt(e)
-                    });
-                var a = template("order-list-tmpl", t);
-                if (reset) {
-                    reset = false;
-                    $("#order-list").html(a)
+                template.helper("$getLocalTime",function(e) {
+                    var t = new Date(parseInt(e) * 1e3);
+                    var r = "";
+                    r += t.getFullYear() + "年";
+                    r += t.getMonth() + 1 + "月";
+                    r += t.getDate() + "日 ";
+                    r += t.getHours() + ":";
+                    r += t.getMinutes();
+                    return r
+                });
+                template.helper("p2f",function(e) {
+                    return (parseFloat(e) || 0).toFixed(2);
+                });
+                template.helper("parseInt",function(e) {
+                    return parseInt(e);
+                });
+                var r = template("order-list-tmpl", t);
+
+                if (!reset) {
+                    $("#order-list").append(r);
                 } else {
-                    $("#order-list").append(a)
+                    $("#order-list").html(r);
                 }
             }
         })
     }
-
-    $.ajax({
-        type: "get",
-        url: ApiUrl + "/Memberpayment/payment_list.html",
-        data: {
-            key: e
-        },
-        dataType: "json",
-        success: function (e) {
-            var r = {};
-            $.each(e && e.result && e.result.payment_list || [],
-                function (e, t) {
-                    r[t] = true
-                });
-            var o = navigator.userAgent.match(/MicroMessenger\/(\d+)\./);
-            if (parseInt(o && o[1] || 0) >= 5) {
-                if (r.wxpay_jsapi) {
-                    a = true
-                }
-            } else {
-                if (r.alipay_h5) {
-                    t = true
-                }
-            }
-        }
-    });
-    r(page, curpage);
-    $(window).scroll(function () {
-        if ($(window).scrollTop() + $(window).height() > $(document).height() - 1) {
-            r()
-        }
-    });
+    $("#order-list").on("click", ".cancel-order", r);
+    $("#order-list").on("click", ".delete-order", o);
+    $("#order-list").on("click", ".sure-order", n);
+    $("#order-list").on("click", ".evaluation-order", l);
+    $("#order-list").on("click", ".evaluation-again-order", d);
+    $("#order-list").on("click", ".viewdelivery-order", c);
     $("#order-list").on("click", ".check-payment",
-        function () {
-            var e = $(this).attr("data-paySn");
-            toPay(e, "membervrbuy", "pay");
-            return false
-        });
-    $("#order-list").on("click", ".cancel-order", o);
-
-    function o() {
+    function() {
+        var e = $(this).attr("data-paySn");
+        toPay(e, "memberbuy", "pay");
+        return false;
+    });
+    function r() {
         var e = $(this).attr("order_id");
         layer.open({
             content: '确定取消订单？'
+            , btn: ['确定','取消']
+            , yes: function (index) {
+                a(e)
+                layer.close(index);
+            }
+        });
+    }
+    function a(r) {
+        $.ajax({
+            type: "post",
+            url: ApiUrl + "/Memberorder/order_cancel.html",
+            data: {
+                order_id: r,
+                key: e
+            },
+            dataType: "json",
+            success: function(e) {
+                if (e.code == 10000) {
+                    reset = true;
+                    t()
+                } else {
+                    layer.open({content: e.message, btn: '我知道了'});
+                }
+            }
+        })
+    }
+    function o() {
+        var e = $(this).attr("order_id");
+        layer.open({
+            content: '是否移除订单？电脑端订单回收站可找回订单！'
             , btn: ['确定','取消']
             , yes: function (index) {
                 i(e)
@@ -143,40 +142,82 @@ $(function () {
             }
         });
     }
-
-    function i(t) {
+    function i(r) {
         $.ajax({
             type: "post",
-            url: ApiUrl + "/Membervrorder/order_cancel.html",
+            url: ApiUrl + "/Memberorder/order_delete.html",
             data: {
-                order_id: t,
+                order_id: r,
                 key: e
             },
             dataType: "json",
-            success: function (e) {
+            success: function(e) {
                 if (e.code == 10000) {
-                    r(page, curpage)
+                    reset = true;
+                    t()
                 } else {
-                    layer.open({content: e.message,skin: 'msg',time: 2});
+                    layer.open({content: e.message, btn: '我知道了'});
                 }
             }
         })
     }
-
-    $("#order-list").on("click", ".evaluation-order",
-        function () {
-            var e = $(this).attr("order_id");
-            location.href = WapSiteUrl + "/member/member_vr_evaluation.html?order_id=" + e
+    function n() {
+        var e = $(this).attr("order_id");
+        layer.open({
+            content: '确定收到了货物吗？'
+            , btn: ['确定','取消']
+            , yes: function (index) {
+                s(e)
+                layer.close(index);
+            }
         });
-    $("#filtrate_ul").find("a").click(function () {
+    }
+    function s(r) {
+        $.ajax({
+            type: "post",
+            url: ApiUrl + "/Memberorder/order_receive.html",
+            data: {
+                order_id: r,
+                key: e
+            },
+            dataType: "json",
+            success: function(e) {
+                if (e.code == 10000) {
+                    reset = true;
+                    t()
+                } else {
+                    layer.open({content: e.message, btn: '我知道了'});
+                }
+            }
+        })
+    }
+    function l() {
+        var e = $(this).attr("order_id");
+        location.href = WapSiteUrl + "/member/member_evaluation.html?order_id=" + e
+    }
+    function d() {
+        var e = $(this).attr("order_id");
+        location.href = WapSiteUrl + "/member/member_evaluation_again.html?order_id=" + e
+    }
+    function c() {
+        var e = $(this).attr("order_id");
+        location.href = WapSiteUrl + "/member/order_delivery.html?order_id=" + e
+    }
+    $("#filtrate_ul").find("a").click(function() {
         $("#filtrate_ul").find("li").removeClass("selected");
         $(this).parent().addClass("selected").siblings().removeClass("selected");
         reset = true;
         window.scrollTo(0, 0);
-        r()
+        t();
+    });
+    t();
+    $(window).scroll(function() {
+        if ($(window).scrollTop() + $(window).height() > $(document).height() - 10) {
+            //console.log($(window).scrollTop() + $(window).height());
+            t();
+        }
     })
 });
-
 function get_footer() {
     if (!footer) {
         footer = true;
